cf430a047684169e04886abb2238cf6019192752
Kfuente <kevenfuentes@42.240.21.198.tigernet.wifi.dyn.clemson.edu>
2017-04-11 11:25:23 -0400

    Change the ways that we access the file
    We instead map it into memory using mmap in order to go through the
    file as if it were an array. Way better!

--- test.c
+++ test.c
@@ -2,38 +2,48 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
-
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <sys/mman.h>
 
 int main(){
-  FILE *fp;
- int c;
-
- fp = fopen("random.img","r");
-
- fseek(fp,9728,SEEK_SET);
-  c = fgetc(fp);
-
-
-    printf("%x", c);
-
- fclose(fp);
-
-
- //Check to see if attribute byte is 20
-
+    
+    //Get the file descriptor for the image
+    int fd = open("random.img",O_RDWR, S_IRUSR | S_IWUSR);
+    struct stat sb;
+    
+    //Get the side of the file and saave it to sb
+    if (fstat(fd,&sb) == -1){
+        perror("couldn't get file size.\n");
+    }
+    
+    //Map the file into memory
+    char *filemappedpage = mmap(NULL, sb.st_size,
+                                PROT_READ | PROT_WRITE,
+                                MAP_SHARED, fd, 0);
+    
+    // Printing the 9728 byte
+    printf("%x",filemappedpage[9728]);
+    
+    
+    
+    
+    //Check to see if attribute byte is 20
+    
     //if 20 then it is a file else if it is 10 it is a directory
-
-        //then look at last four bytes to get size and divide by 512 to get number of clusters.
-        //make array of size of clusters
-        //go to FAT and follow it until EOF to fill in array with the cluster numbers
-        //then calculate location of each cluster and write 512 bytes to file
-
-
-
-
-
-
-
- return(0);
-
+    
+    //then look at last four bytes to get size and divide by 512 to get number of clusters.
+    //make array of size of clusters
+    //go to FAT and follow it until EOF to fill in array with the cluster numbers
+    //then calculate location of each cluster and write 512 bytes to file
+    
+    
+    
+    
+    
+    
+    
+    return(0);
+    
 }
